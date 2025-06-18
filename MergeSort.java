e3542e2a8c799c3d9c80c2a52246e8afbb73fcd9
comlibmb <1844410276@qq.com>
2025-06-18 12:42:40 +0800

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -1,5 +1,7 @@
 package class01;
 
+import java.util.Random;
+
 /**
  * @author pacai
  * @version 1.0
@@ -92,6 +94,7 @@ public class MergeSort {
                 +
                 merge1(arr, L, mid, R);
     }
+
     @SuppressWarnings("all")
     private static int merge1(int[] arr, int L, int M, int R) {
         int[] help = new int[R - L + 1];
@@ -99,7 +102,7 @@ public class MergeSort {
         int p2 = M + 1;
         int ans = 0;
         int i = 0;
-        while(p1 <= M && p2 <= R){
+        while (p1 <= M && p2 <= R) {
             //todo 利用有序信息一次性把小和求出
             ans += arr[p1] < arr[p2] ? (R - L + 1) * arr[p1] : 0; //加上右边所有比左边大的数产生的小和数
             help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
@@ -117,4 +120,73 @@ public class MergeSort {
         return ans;
     }
 
+
+    //逆序对
+    public static void mergeSort3(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        process3(arr, 0, arr.length - 1);
+    }
+
+    private static int process3(int[] arr, int L, int R) {
+        if (L == R) {
+            return 0;
+        }
+        int mid = L + ((R - L) >> 1);
+        return process3(arr, L, mid)
+                +
+                process3(arr, mid + 1, R)
+                +
+                merge3(arr, L, mid, R);
+    }
+
+    private static int merge3(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int p1 = L;
+        int p2 = M + 1;
+        int i = 0;
+        int ans = 0;
+        while(p1 <= M && p2 <= R){
+            ans += arr[p2] < arr[p1] ? R - L + 1 : 0;
+            help[i++] = arr[p1] >= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+        return ans;
+    }
+
+    //对数器
+    public static int cal(int[] arr){
+        if(arr == null || arr.length < 2){
+            return 0;
+        }
+        int ans = 0;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1,num = arr[j]; j < arr.length - 1; j++) {
+                if(arr[j + 1] < num){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    //随机数组·
+    private static int[] randomArray(int maxValue,int maxSize){
+        Random random = new Random();
+        int[] arr = new int[random.nextInt(maxSize)];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(maxValue);
+        }
+        return arr;
+    }
 }
