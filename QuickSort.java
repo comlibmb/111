681704ac4c86280d8195ad9ed5b1ddd4a4062489
comlibmb <1844410276@qq.com>
2025-06-20 13:18:23 +0800

    Add files via upload

--- QuickSort.java
+++ QuickSort.java
@@ -11,6 +11,44 @@ public class QuickSort {
         arr[j] = temp;
     }
 
+
+    public static void quickSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        process1(arr, 0, arr.length - 1);
+    }
+
+
+    public static void process1(int[] arr, int L, int R) {
+        if (L > R) {
+            return;
+        }
+        int mid = partition(arr, L, R);
+        process1(arr, L, mid - 1);
+        process1(arr, mid + 1, R);
+    }
+
+    public static void quickSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+
+    }
+
+    public static void process2(int[] arr, int L, int R) {
+        if (L > R) {
+            return;
+        }
+
+        int[] midArr = netherlandsFlag1(arr, 0, arr.length - 1);
+        process2(arr, L, midArr[0] - 1);
+        process2(arr, midArr[1] + 1, R);
+    }
+
+
     public static int partition(int[] arr, int L, int R) {
         if (L > R) {
             return -1;
@@ -31,7 +69,7 @@ public class QuickSort {
     }
 
     @SuppressWarnings("all")
-    public static int[] netherlandsFlag(int[] arr, int L, int R) {
+    public static int[] netherlandsFlag1(int[] arr, int L, int R) {
         if (L > R) {
             return new int[]{-1, -1};
         }
@@ -54,4 +92,27 @@ public class QuickSort {
         return new int[]{less + 1, more};
     }
 
+
+    public static int[] netherlandsFlag2(int[] arr, int L, int R) {
+        if (L > R) {
+            return new int[]{-1, -1};
+        }
+        if (L == R) {
+            return new int[]{L, R};
+        }
+        int less = L - 1;
+        int index = L;
+        int more = R + 1;
+        int val = arr[R];
+        while (index < more) {
+            if (arr[index] == val) {
+                index++;
+            } else if (arr[index] > val) {
+                swap(arr, index, --more);
+            } else {
+                swap(arr, index++, ++less);
+            }
+        }
+        return new int[]{less + 1, more};
+    }
 }
