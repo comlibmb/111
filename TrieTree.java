8f1d7f03721618aaac37edc3cf344e48a515cb04
comlibmb <1844410276@qq.com>
2025-06-29 14:52:04 +0800

    Add files via upload

--- TrieTree.java
+++ TrieTree.java
@@ -4,6 +4,7 @@ package class01;
  * @author pacai
  * @version 1.0
  */
+@SuppressWarnings("all")
 public class TrieTree {
     private static class Node{
         private int pass;
@@ -23,7 +24,7 @@ public class TrieTree {
         public Trie1() {
             root = new Node();
         }
-        @SuppressWarnings("all")
+
         public void insert(String word){
             if(word == null){
                 return;
@@ -41,5 +42,59 @@ public class TrieTree {
             }
             cur.end++;
         }
+
+        //word加入几次
+        public int search(String word){
+            if(word == null){
+                return 0;
+            }
+            Node cur = root;
+            int path;
+            for (int i = 0; i < word.length(); i++) {
+                path = word.charAt(i) - 'a';
+                if(cur.next[path] == null){
+                    return 0;
+                }
+                cur = cur.next[path];
+            }
+            return cur.end;
+        }
+
+        //所有加入的字符串，有多少以pre作为前缀
+        public int prefixSearch(String pre){
+            if(pre == null){
+                return 0;
+            }
+            Node cur = root;
+            int path;
+            for (int i = 0; i < pre.length(); i++) {
+                path = pre.charAt(i) - 'a';
+                if(cur.next[path] == null){
+                    return 0;
+                }
+                cur = cur.next[path];
+            }
+            return cur.pass;
+        }
+
+        //删除
+        public void delete(String word){
+            if(word == null || search(word) == 0){ // 确保有加入该字符串才可以执行删除
+                return;
+            }
+            Node cur = root;
+            cur.pass--;
+            int path;
+            for (int i = 0; i < word.length(); i++) {
+                path = word.charAt(i) - 'a';
+                if(--cur.next[path].pass == 0){
+                    cur.next[path] = null;
+                    return;
+                }
+                cur = cur.next[path];
+            }
+            cur.end--;
+        }
+
     }
 }
